d14d9a44ebff7841cdbed1be28fdf1cf22382246
Vuzeta <machniik95@gmail.com>
2019-07-02 16:47:34 +0200

    update

---
+++ src/components/ModalSuccess.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ModalSuccess = () => {
+	return (
+		<div class="modalSuccess">
+			<h1>
+				<i className="large material-icons">group_add</i> Dodano pracownika
+			</h1>
+		</div>
+	);
+};
+
+export default ModalSuccess;
--- src/pages/AddEmployee/AddEmployee.js
+++ src/pages/AddEmployee/AddEmployee.js
@@ -4,36 +4,38 @@ import AddEmployeeForm from '../../components/AddEmployeeForm';
 import AppContext from '../../context';
 import { Prompt } from 'react-router-dom';
 import ErrorMessageAddEmployee from '../../components/ErrorMessageAddEmployee';
+import ModalSuccess from '../../components/ModalSuccess';
 
 class AddEmployee extends Component {
-  componentWillUnmount() {
-    this.props.clearForm();
-  }
-  render() {
-    return (
-      <AppContext.Consumer>
-        {context => (
-          <div className="addEmployee">
-            <h1 className="page-title">Dodaj pracownika</h1>
-            <ErrorMessageAddEmployee />
-            <AddEmployeeForm submit={context.handleSubmit} date={context.handleDate} />
-            <div />
-            <Prompt
-              when={
-                context.firstName ||
-                context.lastName ||
-                context.email ||
-                context.phone ||
-                context.accountNumber ||
-                context.rate
-              }
-              message="Czy napewno chcesz porzucić wprowadzone dane?"
-            />
-          </div>
-        )}
-      </AppContext.Consumer>
-    );
-  }
+	componentWillUnmount() {
+		this.props.clearForm();
+	}
+	render() {
+		return (
+			<AppContext.Consumer>
+				{context => (
+					<div className="addEmployee">
+						<h1 className="page-title">Dodaj pracownika</h1>
+						{context.formCompleted ? <ModalSuccess /> : null}
+						<ErrorMessageAddEmployee />
+						<AddEmployeeForm submit={context.handleSubmit} date={context.handleDate} />
+						<div />
+						<Prompt
+							when={
+								context.firstName ||
+								context.lastName ||
+								context.email ||
+								context.phone ||
+								context.accountNumber ||
+								context.rate
+							}
+							message="Czy napewno chcesz porzucić wprowadzone dane?"
+						/>
+					</div>
+				)}
+			</AppContext.Consumer>
+		);
+	}
 }
 
 export default AddEmployee;
