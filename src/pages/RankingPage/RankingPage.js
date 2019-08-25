f3ae386dccbe2e2cebd5130e3ca6d205fcd05339
Vuzeta95 <machnio95@o2.pl>
2019-08-25 15:28:19 +0200

    add charts in RankingPage

--- src/pages/RankingPage/RankingPage.js
+++ src/pages/RankingPage/RankingPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './RankingPage.scss';
+import ChartMakingMoney from '../../components/ChartMakingMoney/ChartMakingMoney';
+import ChartEarnedMoney from '../../components/ChartEarnedMoney/ChartEarnedMoney';
+import ChartWorkedHours from '../../components/ChartWorkedHours/ChartWorkedHours';
 
 const RankingPage = () => {
   return (
     <div>
-      <h1 className="page-title">Ranking Page</h1>
+      <h1 className="page-title">Ranking Pracowników</h1>
+      <ChartMakingMoney />
+      <ChartEarnedMoney />
+      <ChartWorkedHours />
     </div>
   );
 };
