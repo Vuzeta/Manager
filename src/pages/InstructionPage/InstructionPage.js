50d13b19ef9dd4000fdae26a35eaeb23e3e20573
Vuzeta95 <machnio95@o2.pl>
2019-09-04 19:24:49 +0200

    instructionPage update

--- src/pages/InstructionPage/InstructionPage.js
+++ src/pages/InstructionPage/InstructionPage.js
@@ -25,7 +25,7 @@ const InstructionPage = () => {
         </ul>
         <div id="aplication" className="section">
           <h2 className="section-title">1. Aplikacja</h2>
-          <p>
+          <p class="paragraph">
             Aplikacja <em>"Manager"</em> służy do zarządzania pracownikami. W aplikacji możemy w
             łatwy sposób dodawać pracownika, modyfikować,
             <br /> otrzymać dostęp do danych, dodawać dzień w jakim pracownik był w pracy,
@@ -34,37 +34,59 @@ const InstructionPage = () => {
         </div>
         <div id="addEmployee" className="section">
           <h2 className="section-title">2. Sekcja "Dodaj Pracownika"</h2>
-          <p>
+          <p class="paragraph--small">
             W sekcji pracownika możemy poprzez wprowadzenia danych dodać pracownika do listy
             "Pracownicy".
           </p>
-          <ul>
+          <ul className="instruction__list">
             <li>
-              Pole "Imię" - Pole musi składać się od 1 do maksymalnie 30 znaków oraz nie może
-              posiadać między znakami spacji.
+              <i className="material-icons prefix instruction__icon">account_circle</i>
+              <p class="paragraph--small">
+                Pole "Imię" - Pole musi składać się od 1 do maksymalnie 30 znaków oraz nie może
+                posiadać między znakami spacji.
+              </p>
             </li>
             <li>
-              Pole "Nazwisko" - Pole musi składać się od 1 do maksymalnie 30 znaków oraz nie może
-              posiadać między znakami spacji.
+              <i className="material-icons prefix instruction__icon">account_circle</i>
+              <p class="paragraph--small">
+                Pole "Nazwisko" - Pole musi składać się od 1 do maksymalnie 30 znaków oraz nie może
+                posiadać między znakami spacji.
+              </p>
             </li>
             <li>
-              Pole "Email" - Pole musi posiadać znak "@" oraz po niej adres domeny w jakiej się
-              znajduje np. "gmail.com".
+              <i className="material-icons prefix instruction__icon">email</i>
+              <p class="paragraph--small">
+                Pole "Email" - Pole musi posiadać znak "@" oraz po niej adres domeny w jakiej się
+                znajduje np. "gmail.com".
+              </p>
             </li>
-            <li>Pole "Telefon" - Pole musi składać się z 9 cyfr.</li>
-            <li>Pole "Numer konta bankowego" - Pole musi składać się z 26 cyfr.</li>
             <li>
-              Pole "Stawka netto" - Pole musi zawierać minimum stawkę 9zł według Kodeksu Pracy z
-              2019r.
+              <i className="material-icons prefix instruction__icon">phone</i>
+              <p class="paragraph--small">Pole "Telefon" - Pole musi składać się z 9 cyfr.</p>
+            </li>
+            <li>
+              <i className="material-icons prefix instruction__icon">credit_card</i>
+              <p class="paragraph--small">
+                Pole "Numer konta bankowego" - Pole musi składać się z 26 cyfr.
+              </p>
+            </li>
+            <li>
+              <i className="material-icons prefix instruction__icon">attach_money</i>
+              <p class="paragraph--small">
+                Pole "Stawka netto" - Pole musi zawierać minimum stawkę 9zł według Kodeksu Pracy z
+                2019r.
+              </p>
             </li>
           </ul>
-          <p>
+          <p class="paragraph">
             Przycisk dodaj pracownika - po wciśnięciu formularz zostaje sprawdzony pod kątem
             wprowadzonych danych. Jeśli formularz został źle wypełniony zostanie wyświetlony
             odpowiedni komunikat
           </p>
-          <p>Jeśli formularz zostanie wypełniony prawidłowo zostanie wyświetlone powiadomienie.</p>
-          <p>
+          <p class="paragraph">
+            Jeśli formularz zostanie wypełniony prawidłowo zostanie wyświetlone powiadomienie.
+          </p>
+          <p class="paragraph">
             Jeśli wprowadzając dane przypadkowo kliknięmy na inną sekcje wyświetlony zostanie
             komunikat "Czy napewno chcesz porzucić wprowadzone dane?"
           </p>
@@ -75,15 +97,15 @@ const InstructionPage = () => {
         </div>
         <div id="employees" className="section">
           <h2 className="section-title">3. Sekcja Pracownicy</h2>
-          <p>
+          <p class="paragraph">
             Sekcja pracownicy służy do wyświetlania listy pracowników. Dzięki filtrowaniu możemy
             uzyskać dostęp do interesującego nas pracownika.
           </p>
-          <p>
+          <p class="paragraph">
             Filtrowanie odpywa się poprzez wpisanie w dane pole ciąg znaków, poprzez który zostanie
             wyświetlona przefiltrowana lista pracowników.
           </p>
-          <p>
+          <p class="paragraph">
             Filtrowanie można ze sobą łączyć wprowadzająć dane w kilku polach. Ułatwi nam to
             znalezienie konkretnego pracownika na liście.
           </p>
@@ -106,7 +128,9 @@ const InstructionPage = () => {
         </div>
         <div id="employeePanel" className="section">
           <h2 className="section-title">4. Panel Pracownika</h2>
-          <p>W Panelu Pracownika mamy dostęp do szczegółowych danych pracownika.</p>
+          <p class="paragraph">
+            W Panelu Pracownika mamy dostęp do szczegółowych danych pracownika.
+          </p>
           <ul>
             <li>Imię - wyświetlane jest imię pracownika</li>
             <li>Nazwisko - wyświetlane jest nazwisko pracownika</li>
@@ -120,7 +144,7 @@ const InstructionPage = () => {
             </li>
             <li>Suma przerobionych godzin - wyświetlana jest suma przepracowanych godzin</li>
           </ul>
-          <p>
+          <p class="paragraph">
             <b>Edycja Pracownika</b>
           </p>
           <ul>
@@ -136,10 +160,10 @@ const InstructionPage = () => {
             <li>Nie - wracamy do edycji</li>
             <li>Anuluj Zmiany - Wyłącza tryb edycji</li>
           </ul>
-          <p>
+          <p class="paragraph">
             <b>Usuwanie Pracownika</b>
           </p>
-          <p>
+          <p class="paragraph">
             Usuń pracownika - Po wciśnięciu przycisku zostanie wyświetlony komunikat "Czy napewno
             chcesz usunąć pracownika?"
           </p>
@@ -150,7 +174,7 @@ const InstructionPage = () => {
             </li>
             <li>Nie-Pracownik nie zostanie usunięty</li>
           </ul>
-          <p>
+          <p class="paragraph">
             <b>Dodawanie dnia pracy</b>
           </p>
           <ul>
@@ -167,8 +191,10 @@ const InstructionPage = () => {
               dodane do listy przepracowanych dni.
             </li>
           </ul>
-          <p>! Nie można wprowadzić dnia, który już występuje na liście przepracowanych dni.</p>
-          <p>
+          <p class="paragraph">
+            ! Nie można wprowadzić dnia, który już występuje na liście przepracowanych dni.
+          </p>
+          <p class="paragraph">
             <b>Lista Dni pracy</b>
           </p>
           <ul>
@@ -185,20 +211,20 @@ const InstructionPage = () => {
             </li>
             <li>Usuń - Usunięcie danego dnia</li>
           </ul>
-          <p>
+          <p class="paragraph">
             <b>Wykres</b>
           </p>
-          <p>
+          <p class="paragraph">
             Wykres wyświetla nam 31 najaktualniejszych dni na podstawie listy przepracowanych dni
           </p>
         </div>
         <div id="ranking" className="section">
           <h2 className="section-title">5. Ranking</h2>
-          <p>
+          <p class="paragraph">
             Ranking pozwala nam na uzyskanie danych 5 pracowników, którzy najlepiej zarabiają,
             najwięcej zarobili oraz którzy przepracowali najwięcej godzin.
           </p>
-          <p>Rankig przestawiony jest na wykresie ciasteczkowym.</p>
+          <p class="paragraph">Rankig przestawiony jest na wykresie ciasteczkowym.</p>
         </div>
       </div>
     </div>
