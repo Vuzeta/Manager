af1f3fb7f57ee20cd491dd13da24b1a462f4a47a
Vuzeta95 <machnio95@o2.pl>
2019-09-07 19:02:16 +0200

    Updates

---
+++ precache-manifest.5bb895b45dd2865264c5a319bffec4dd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30a2c49081bc83a4f5f9f17f530e8c8",
+    "url": "/Manager/index.html"
+  },
+  {
+    "revision": "872441e9d325e7eaef9c",
+    "url": "/Manager/static/css/2.1ae2f214.chunk.css"
+  },
+  {
+    "revision": "354e7496912072ffde4b",
+    "url": "/Manager/static/css/main.8bf862b3.chunk.css"
+  },
+  {
+    "revision": "872441e9d325e7eaef9c",
+    "url": "/Manager/static/js/2.d6f4c9f2.chunk.js"
+  },
+  {
+    "revision": "354e7496912072ffde4b",
+    "url": "/Manager/static/js/main.614aef6e.chunk.js"
+  },
+  {
+    "revision": "3fc87d52f8360a0bb2f4",
+    "url": "/Manager/static/js/runtime~main.6825958c.js"
+  },
+  {
+    "revision": "67f618bad2b266dec3fe808705f1c13c",
+    "url": "/Manager/static/media/addSuccessImg.67f618ba.jpg"
+  },
+  {
+    "revision": "050c94dce2684ffc266896600d918b17",
+    "url": "/Manager/static/media/github_icon.050c94dc.svg"
+  },
+  {
+    "revision": "f56f44e5e769fb5234a73e8497dc66fd",
+    "url": "/Manager/static/media/linkedIn_icon.f56f44e5.svg"
+  },
+  {
+    "revision": "e631344b2013a385b6abcd4e37421f98",
+    "url": "/Manager/static/media/logo.e631344b.svg"
+  },
+  {
+    "revision": "08535e4d53b5546c7148bb55ed80f852",
+    "url": "/Manager/static/media/website_icon.08535e4d.svg"
+  }
+]);
